61c7f7f83a617dc65009bf4cb4af545c08b0983f
saddavi <saddavi@live.com>
2025-05-01 06:42:50 +0300

    Update outputs to include frontend URL and API endpoints for serverless architecture

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,25 @@
+# terraform/outputs.tf
 output "dev_vpc_id" {
   value       = google_compute_network.vpc_dev.id
   description = "The ID of the development VPC network"
 }
 
-output "dev_subnet_id" {
-  value       = google_compute_subnetwork.subnet_dev.id
-  description = "The ID of the development subnet"
+output "prod_vpc_id" {
+  value       = google_compute_network.vpc_prod.id
+  description = "The ID of the production VPC network"
 }
 
-output "dev_subnet_cidr" {
-  value       = google_compute_subnetwork.subnet_dev.ip_cidr_range
-  description = "The CIDR range of the development subnet"
+output "dev_api_url" {
+  value       = google_cloud_run_service.api_dev.status[0].url
+  description = "The URL of the development API"
+}
+
+output "prod_api_url" {
+  value       = google_cloud_run_service.api_prod.status[0].url
+  description = "The URL of the production API"
+}
+
+output "frontend_url" {
+  value       = "https://storage.googleapis.com/${google_storage_bucket.frontend.name}/index.html"
+  description = "The URL of the frontend website"
 }
